154ef6b5897b5a1e536069c6e25f406732ba49eb
nohum <eiskalterengel@truthfactory.tk>
2015-05-17 00:00:03 +0200

    socket connection example

---
+++ app/src/main/java/at/fhj/demo/AsyncSocketTask.java
@@ -0,0 +1,32 @@
+package at.fhj.demo;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class AsyncSocketTask extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String... urls) {
+
+        try {
+            Socket socket = new Socket();
+            socket.connect(new InetSocketAddress("google.com", 80));
+
+            if (socket.isConnected()) {
+                // do something
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+    }
+}
