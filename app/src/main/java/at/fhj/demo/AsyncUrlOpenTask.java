20337b1825da4e943be1d5d97e5946ea0960ae1f
nohum <eiskalterengel@truthfactory.tk>
2015-05-17 00:03:03 +0200

    added url open example

---
+++ app/src/main/java/at/fhj/demo/AsyncUrlOpenTask.java
@@ -0,0 +1,37 @@
+package at.fhj.demo;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AsyncUrlOpenTask extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String... urls) {
+
+        try {
+            URL website = new URL("https://www.google.com");
+            HttpURLConnection connection = (HttpURLConnection) website.openConnection();
+
+            InputStream websiteData = connection.getInputStream();
+
+            // ...
+
+            connection.disconnect(); // should be done in finally-block
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+    }
+}
