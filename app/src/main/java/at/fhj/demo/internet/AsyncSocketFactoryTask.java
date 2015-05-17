eb9d057777fb42c4f7576b9333443b5e6737913f
nohum <eiskalterengel@truthfactory.tk>
2015-05-17 21:57:29 +0200

    lint examples refined

---
+++ app/src/main/java/at/fhj/demo/internet/AsyncSocketFactoryTask.java
@@ -0,0 +1,38 @@
+package at.fhj.demo.internet;
+
+import android.os.AsyncTask;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import javax.net.SocketFactory;
+
+public class AsyncSocketFactoryTask extends AsyncTask<String, Void, String> {
+
+    @Override
+    protected String doInBackground(String... urls) {
+        if (urls.length < 1) {
+            throw new IllegalArgumentException("a URL is required");
+        }
+
+        SocketFactory factory = SocketFactory.getDefault();
+
+        try {
+            Socket falsePositive = factory.createSocket();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            Socket shouldBeDetected = factory.createSocket(urls[0], 80);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+    }
+}
