909871c7415c2dd3468974b8c7573678f2526308
nohum <eiskalterengel@truthfactory.tk>
2015-05-20 12:09:12 +0200

    added further examples (also for data-flow analysis)

---
+++ app/src/main/java/at/fhj/demo/GpsStatusActivity.java
@@ -0,0 +1,80 @@
+package at.fhj.demo;
+
+import android.app.Activity;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+public class GpsStatusActivity extends Activity implements LocationListener, GpsStatus.Listener {
+
+    private static final int MILLISECONDS_BETWEEN_UPDATES = 5000;
+    private static final int MIN_METERS_MOVED = 20;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        // false positive
+        locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        // real match
+        locationManager.isProviderEnabled("gps");
+
+        // should be detected with data-flow analysis
+        locationManager.isProviderEnabled(getPossibleProvider());
+
+        // should be detected with data-flow analysis
+        locationManager.isProviderEnabled(getPossibleProviderInverse());
+
+        // real match with FINE permission commented out
+        locationManager.addGpsStatusListener(this);
+
+        // false positive
+        locationManager.removeGpsStatusListener(this);
+    }
+
+    private String getPossibleProvider() {
+        if (MILLISECONDS_BETWEEN_UPDATES > 10000) {
+            return LocationManager.NETWORK_PROVIDER;
+        }
+
+        return LocationManager.GPS_PROVIDER;
+    }
+
+    private String getPossibleProviderInverse() {
+        if (MILLISECONDS_BETWEEN_UPDATES > 10000) {
+            return LocationManager.GPS_PROVIDER;
+        }
+
+        return LocationManager.NETWORK_PROVIDER;
+    }
+
+    @Override
+    public void onGpsStatusChanged(int event) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+}
